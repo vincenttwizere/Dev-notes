585ea06ea0633d5830232b20a78addf8dfab6f3d
vincenttwizere <v.twizere@alustudent.com>
2025-01-04 23:05:53 +0300

    declaring a function

---
+++ js course/Js basics/function.js
@@ -0,0 +1,9 @@
+//function is a group of resuable code that performs a specific task. It can be called multiple times to perform the same task multiple times.
+
+// 1. function declaration
+
+function greet(name) {
+    console.log("Hello, " + name);
+  }
+  
+  greet("John"); // Output: Hello, John  
